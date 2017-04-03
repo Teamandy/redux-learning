c9d478903aa7f63db4e849809ee676c19d0ad7e8
Teamandy <andytimirov@yandex.ru>
2017-04-03 10:29:49 +0300

    pushed a barebone setup

---
+++ readme.md
@@ -0,0 +1,1 @@
+Next learning step - dive into redux
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var SRC = path.resolve(__dirname, 'src');
+var DIST = path.resolve(__dirname, 'dist');
+var config = {
+	entry: SRC + '/app.js',
+	output: {
+		filename: 'app.bundle.js',
+		path: DIST 
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: SRC,
+				use: [
+				{
+					loader: 'babel-loader',
+					options: {
+						presets: ['react', 'es2015'] 
+					}
+				}
+				]
+			},
+			{
+				test: /\.scss$/,
+				include: SRC,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+          			use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+    		template: './src/index.html',
+    		hash: true,
+    		minify: {
+    			collapseWhitespace: true
+    		} 
+		}),
+		new ExtractTextPlugin('app.css'),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.optimize.UglifyJsPlugin()
+	],
+	devServer: {
+		historyApiFallback: true,
+		contentBase: DIST,
+		hot: true
+	}
+};
+
+module.exports = config;
